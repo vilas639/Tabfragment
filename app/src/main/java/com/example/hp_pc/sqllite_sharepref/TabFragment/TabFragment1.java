80198cbd0632b20bc1e80b2508ee9f6d4fdbfbf4
vilas639 <vilasjdhv639@gmail.com>
2018-10-18 00:04:23 +0530

    pass data one tab to another

--- app/src/main/java/com/example/hp_pc/sqllite_sharepref/TabFragment/TabFragment1.java
+++ app/src/main/java/com/example/hp_pc/sqllite_sharepref/TabFragment/TabFragment1.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.hp_pc.sqllite_sharepref.R;
 
@@ -14,6 +16,8 @@ import com.example.hp_pc.sqllite_sharepref.R;
  */
 public class TabFragment1 extends Fragment {
 
+    private EditText editText;
+    private Button btnSendData;
 
     public TabFragment1() {
         // Required empty public constructor
@@ -24,8 +28,23 @@ public class TabFragment1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView = inflater.inflate(R.layout.fragment_tab_fragment1, container, false);
-        return rootView;
+        View view = inflater.inflate(R.layout.fragment_tab_fragment1, container, false);
+        editText = (EditText) view.findViewById(R.id.et_name);
+        btnSendData = (Button) view.findViewById(R.id.btn_send);
+        btnSendData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TabFragment2 tabFragment2 =new TabFragment2();
+
+                Bundle bundle = new Bundle();
+                bundle.putString("username",editText.getText().toString());
+                tabFragment2.setArguments(bundle);
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.second_frag,tabFragment2).commit();
+
+            }
+        });
+
+        return view;
     }
 
 }
--- app/src/main/java/com/example/hp_pc/sqllite_sharepref/TabFragment/TabFragment2.java
+++ app/src/main/java/com/example/hp_pc/sqllite_sharepref/TabFragment/TabFragment2.java
@@ -6,6 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.hp_pc.sqllite_sharepref.R;
 
@@ -14,7 +17,7 @@ import com.example.hp_pc.sqllite_sharepref.R;
  */
 public class TabFragment2 extends Fragment {
 
-
+    String value="";
     public TabFragment2() {
         // Required empty public constructor
     }
@@ -24,8 +27,29 @@ public class TabFragment2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View rootView =  inflater.inflate(R.layout.fragment_tab_fragment2, container, false);
-        return  rootView;
+        View view =  inflater.inflate(R.layout.fragment_tab_fragment2, container, false);
+        TextView txt2 = (TextView) view.findViewById(R.id.txtname);
+        Button btnsendthird = (Button) view.findViewById(R.id.btnsend2);
+        Bundle bundle = getArguments();
+        if(bundle!= null)
+        {
+            value = getArguments().getString("username");
+            txt2.setText(value);
+        }
+
+        btnsendthird.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(),""+value,Toast.LENGTH_LONG).show();
+            }
+        });
+        return  view;
     }
 
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Toast.makeText(getContext(),"onResume gets called",Toast.LENGTH_LONG).show();
+    }
 }
